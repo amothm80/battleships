23db12844424c7b0fa9597b41fc7a0ec1aceb951
amothm80 <amo.1980@gmail.com>
2024-11-26 16:29:20 +0300

    game logic

--- src/battleship.js
+++ src/battleship.js
@@ -31,11 +31,24 @@ export class Battelship {
 export class Battleboard {
   constructor() {
     // this.boardP1 = [];
-    this.shipsP1 = []
+    this.shipsP1 = [];
     this.boardP1 = this.createBoard();
     // this.boardP2 = [];
-    this.shipsP2 = []
+    this.shipsP2 = [];
     this.boardP2 = this.createBoard();
+    this.gameOn = false;
+  }
+
+  startGame() {
+    if (this.shipsP1.length == 4 && this.shipsP2 == 4) {
+      this.gameOn = true;
+      return 'Game Started';
+    }else{
+      return "4 ships for each player has to be on board."
+    }
+  }
+  getGameStatus(){
+    return this.gameOn;
   }
 
   createBoard() {
@@ -66,14 +79,16 @@ export class Battleboard {
   }
 
   receiveAttack(player, location) {
-    const playerBoard = this.getBattleboard(player);
-    if (playerBoard[location[0]][location[1]] != null) {
-      playerBoard[location[0]][location[1]].hit();
-      playerBoard[location[0]][location[1]] = 'H';
-    } else {
-      playerBoard[location[0]][location[1]] = 'M';
+    if (this.gameOn) {
+      const playerBoard = this.getBattleboard(player);
+      if (playerBoard[location[0]][location[1]] != null) {
+        playerBoard[location[0]][location[1]].hit();
+        playerBoard[location[0]][location[1]] = 'H';
+      } else {
+        playerBoard[location[0]][location[1]] = 'M';
+      }
+      return this.getPlayerDetails(player);
     }
-    return this.getPlayerDetails(player)
   }
 
   addShipToPlayer(ship, player) {
@@ -87,19 +102,21 @@ export class Battleboard {
   }
 
   addShipToBoard(ship, location, orientation, player) {
-    const playerBoard = player == 1 ? this.boardP1 : this.boardP2;
-    if (this.validateLocation(ship.getlength(), location, orientation)) {
-      for (let i = 0; i < ship.getlength(); i++) {
-        if (orientation == 'H') {
-          playerBoard[location[0]][location[1] + i] = ship;
-        } else if (orientation == 'V') {
-          playerBoard[location[0] + i][location[1]] = ship;
+    if (!this.gameOn) {
+      const playerBoard = player == 1 ? this.boardP1 : this.boardP2;
+      if (this.validateLocation(ship.getlength(), location, orientation)) {
+        for (let i = 0; i < ship.getlength(); i++) {
+          if (orientation == 'H') {
+            playerBoard[location[0]][location[1] + i] = ship;
+          } else if (orientation == 'V') {
+            playerBoard[location[0] + i][location[1]] = ship;
+          }
         }
+        this.addShipToPlayer(ship, player);
       }
-      this.addShipToPlayer(ship, player)
+      return this.getPlayerDetails(player);
+      // return this.getBattleboard(player)
     }
-    return this.getPlayerDetails(player)
-    // return this.getBattleboard(player)
   }
 
   getBattleboard(player) {
@@ -109,17 +126,36 @@ export class Battleboard {
       return this.boardP2;
     }
   }
-  getShips(player){
+  getShips(player) {
     if (player == 1) {
       return this.shipsP1;
     } else if (player == 2) {
       return this.shipsP2;
     }
   }
-  getPlayerDetails(player){
-    return {player:player,
+  getPlayerDetails(player) {
+    return {
+      player: player,
       player_ships: this.getShips(player),
       player_board: this.getBattleboard(player),
+    };
+  }
+  checkWinner() {
+    if (this.gameOn) {
+      const shipsP1sunk = this.getShips(1).filter((ship) => {
+        !ship.isSunk();
+      });
+      const shipsP2sunk = this.getShips(2).filter((ship) => {
+        !ship.isSunk();
+      });
+      if (shipsP1sunk.length == 0) {
+        this.gameOn = false;
+        return 'Player 1 Wins';
+      }
+      if (shipsP2sunk.length == 0) {
+        this.gameOn = false;
+        return 'Player 2 Wins';
+      }
     }
   }
 }
