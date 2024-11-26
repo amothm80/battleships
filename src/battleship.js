f49faade82ccc8158ec2e1fda8ebfa26601c11b6
amothm80 <amo.1980@gmail.com>
2024-11-26 12:32:31 +0300

    receiving attacks and player details

--- src/battleship.js
+++ src/battleship.js
@@ -30,7 +30,11 @@ export class Battelship {
 
 export class Battleboard {
   constructor() {
+    // this.boardP1 = [];
+    this.shipsP1 = []
     this.boardP1 = this.createBoard();
+    // this.boardP2 = [];
+    this.shipsP2 = []
     this.boardP2 = this.createBoard();
   }
 
@@ -47,13 +51,13 @@ export class Battleboard {
 
   validateLocation(length, location, orientation) {
     if (orientation == 'H') {
-      if (location[0] + length - 1 > 9) {
+      if (location[1] + length - 1 > 9) {
         return false;
       } else {
         return true;
       }
     } else if (orientation == 'V') {
-      if (location[1] + length - 1 > 9) {
+      if (location[0] + length - 1 > 9) {
         return false;
       } else {
         return true;
@@ -61,29 +65,61 @@ export class Battleboard {
     }
   }
 
-  addShip(ship, location, orientation, player) {
+  receiveAttack(player, location) {
+    const playerBoard = this.getBattleboard(player);
+    if (playerBoard[location[0]][location[1]] != null) {
+      playerBoard[location[0]][location[1]].hit();
+      playerBoard[location[0]][location[1]] = 'H';
+    } else {
+      playerBoard[location[0]][location[1]] = 'M';
+    }
+    return this.getPlayerDetails(player)
+  }
+
+  addShipToPlayer(ship, player) {
+    if (player == 1) {
+      this.shipsP1.push(ship);
+      return this.shipsP1;
+    } else if (player == 2) {
+      this.shipsP2.push(ship);
+      return this.shipsP2;
+    }
+  }
+
+  addShipToBoard(ship, location, orientation, player) {
+    const playerBoard = player == 1 ? this.boardP1 : this.boardP2;
     if (this.validateLocation(ship.getlength(), location, orientation)) {
       for (let i = 0; i < ship.getlength(); i++) {
         if (orientation == 'H') {
-          if ((player = 1)) {
-            this.boardP1[location[0]][location[1] + i] = ship;
-          } else if ((player = 2)) {
-            this.boardP2[location[0]][location[1] + i] = ship;
-          }
+          playerBoard[location[0]][location[1] + i] = ship;
         } else if (orientation == 'V') {
-          if ((player = 1)) {
-            this.boardP1[location[0] + i][location[1]] = ship;
-          } else if ((player = 2)) {
-            this.boardP2[location[0] + i][location[1]] = ship;
-          }
+          playerBoard[location[0] + i][location[1]] = ship;
         }
       }
+      this.addShipToPlayer(ship, player)
     }
-    if ((player = 1)) {
-        return this.boardP1;
-    } else if ((player = 2)) {
-        return this.boardP2;
-    }
+    return this.getPlayerDetails(player)
+    // return this.getBattleboard(player)
+  }
 
+  getBattleboard(player) {
+    if (player == 1) {
+      return this.boardP1;
+    } else if (player == 2) {
+      return this.boardP2;
+    }
+  }
+  getShips(player){
+    if (player == 1) {
+      return this.shipsP1;
+    } else if (player == 2) {
+      return this.shipsP2;
+    }
+  }
+  getPlayerDetails(player){
+    return {player:player,
+      player_ships: this.getShips(player),
+      player_board: this.getBattleboard(player),
+    }
   }
 }
--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,4 +1,4 @@
-import { Battelship,Battleboard } from './battleship';
+import { Battelship, Battleboard } from './battleship';
 
 describe('Battleship', () => {
   const bs = new Battelship(3);
@@ -17,48 +17,184 @@ describe('Battleship', () => {
   });
 });
 
-describe('Battleboard',()=>{
+describe('Battleboard', () => {
   const bb = new Battleboard();
-  const bs5 = new Battelship(5);
+  const bs3p1 = new Battelship(3);
+  const bs5p1 = new Battelship(5);
+  const bs3p2 = new Battelship(3);
+  const bs5p2 = new Battelship(5);
 
-  test('validate ship length 3', ()=>{
-    expect(bb.validateLocation(3,[0,0],'H')).toBe(true)
-    expect(bb.validateLocation(3,[9,0],'H')).toBe(false)
-    expect(bb.validateLocation(3,[9,0],'V')).toBe(true)
-    expect(bb.validateLocation(3,[0,9],'V')).toBe(false)
+  test('validate ship length 3', () => {
+    expect(bb.validateLocation(3, [0, 0], 'H')).toBe(true);
+    expect(bb.validateLocation(3, [0, 9], 'H')).toBe(false);
+    expect(bb.validateLocation(3, [9, 0], 'V')).toBe(false);
+    expect(bb.validateLocation(3, [0, 9], 'V')).toBe(true);
+  });
+  test('validate ship length 5', () => {
+    expect(bb.validateLocation(5, [0, 5], 'H')).toBe(true);
+    expect(bb.validateLocation(5, [0, 6], 'H')).toBe(false);
+    expect(bb.validateLocation(5, [5, 5], 'V')).toBe(true);
+    expect(bb.validateLocation(5, [6, 5], 'V')).toBe(false);
+  });
+  test('add ship length 3 location 0,0H player 1', () => {
+    expect(bb.addShipToBoard(bs3p1, [0, 0], 'H', 1)).toStrictEqual({
+      player: 1,
+      player_ships: [bs3p1],
+      player_board: [
+        [bs3p1, bs3p1, bs3p1, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+      ],
+    });
+  });
+  test('add ship length 5 location 5,5V player 1', () => {
+    expect(bb.addShipToBoard(bs5p1, [5, 5], 'V', 1)).toEqual({
+      player: 1,
+      player_ships: [bs3p1,bs5p1],
+      player_board: [
+        [bs3p1, bs3p1, bs3p1, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs3p1, null, null, null, null],
+      ],
+    });
+  });
+  test('add ship length 3 location 1,4H player 2', () => {
+    expect(bb.addShipToBoard(bs3p2, [1, 4], 'H', 2)).toEqual({
+      player: 2,
+      player_ships: [bs3p2],
+      player_board: [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, bs3p2, bs3p2, bs3p2, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+      ],
+    });
+  });
+  test('add ship length 5 location 7,2V player 2 invalid location', () => {
+    expect(bb.addShipToBoard(bs5p2, [7, 2], 'V', 2)).toEqual({
+      player: 2,
+      player_ships: [bs3p2],
+      player_board: [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, bs3p2, bs3p2, bs3p2, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+      ],
+    });
+  });
+  test('add ship length 5 location 5,2V player 2', () => {
+    expect(bb.addShipToBoard(bs5p2, [5, 2], 'V', 2)).toEqual({
+      player: 2,
+      player_ships: [bs3p2,bs5p2],
+      player_board: [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, bs3p2, bs3p2, bs3p2, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+      ],
+    });
+  });
 
-  })
-  test('validate ship length 5', ()=>{
-    expect(bb.validateLocation(5,[5,0],'H')).toBe(true)
-    expect(bb.validateLocation(5,[6,0],'H')).toBe(false)
-    expect(bb.validateLocation(5,[5,5],'V')).toBe(true)
-    expect(bb.validateLocation(5,[5,6],'V')).toBe(false)
-  })
-  test('add ship length 5 location 0,0 player 1', ()=>{
-    expect(bb.addShip(bs5,[0,0],'H',1)).toEqual([
-      [bs5,bs5,bs5,bs5,bs5,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null]])
-  })
-  test('add ship length 5 location 5,5, player 1', ()=>{
-    expect(bb.addShip(bs5,[5,5],'V',1)).toEqual([
-      [bs5,bs5,bs5,bs5,bs5,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,null,null,null,null,null],
-      [null,null,null,null,null,bs5,null,null,null,null],
-      [null,null,null,null,null,bs5,null,null,null,null],
-      [null,null,null,null,null,bs5,null,null,null,null],
-      [null,null,null,null,null,bs5,null,null,null,null],
-      [null,null,null,null,null,bs5,null,null,null,null]
-    ])
-  })
-})
+  test('get player 1 board', () => {
+    expect(bb.getBattleboard(1)).toEqual(
+       [
+        [bs3p1, bs3p1, bs3p1, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs3p1, null, null, null, null],
+      ],
+    );
+  });
+
+  test('get player 2 board', () => {
+    expect(bb.getBattleboard(2)).toEqual(
+       [
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, bs3p2, bs3p2, bs3p2, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+        [null, null, bs5p2, null, null, null, null, null, null, null],
+      ],
+    );
+  });
+
+  test('receive attack player 1 location 0,1', () => {
+    expect(bb.receiveAttack(1, [0, 1])).toEqual({
+      player: 1,
+      player_ships: [bs3p1,bs5p1],
+      player_board: [
+        [bs3p1, 'H', bs3p1, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs3p1, null, null, null, null],
+      ],
+    });
+  });
+  test('receive attack player 1 location 1,1', () => {
+    expect(bb.receiveAttack(1, [1, 1])).toEqual({
+      player: 1,
+      player_ships: [bs3p1,bs5p1],
+      player_board: [
+        [bs3p1, 'H', bs3p1, null, null, null, null, null, null, null],
+        [null, 'M', null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs5p1, null, null, null, null],
+        [null, null, null, null, null, bs3p1, null, null, null, null],
+      ],
+    });
+  });
+});
