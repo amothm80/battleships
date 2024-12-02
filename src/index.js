15fddf2d1bcebdde60030a2b7dfbbe617e78a943
amothm80 <amo.1980@gmail.com>
2024-12-02 20:12:09 +0300

    ui

--- src/index.js
+++ src/index.js
@@ -14,13 +14,14 @@ export function renderGameBoards(game) {
   //   griddiv.style.width = squareSize + "px";
   //   griddiv.addEventListener("mouseover", () => {
   const p1boardEl = document.querySelector('#gameboard-1');
-  p1boardEl.innerHTML = ''
+  p1boardEl.innerHTML = '';
   let p1board = game.player1.board.getBattleboard();
   for (let i = 0; i <= 9; i++) {
     for (let j = 0; j <= 9; j++) {
       const griddiv = document.createElement('div');
       griddiv.id = 'p1' + '-' + i + '-' + j;
-      griddiv.className = 'gameboard-cells outline outline-1 outline-slate-200 bg-slate-300 opacity-50 rounded-sm hover:opacity-100 hover:-translate-x-0.5 hover:-translate-y-0.5 transition';
+      griddiv.className =
+        'gameboard-cells outline outline-1 outline-slate-200 bg-slate-300 opacity-50 rounded-sm hover:opacity-100 hover:-translate-x-0.5 hover:-translate-y-0.5 transition';
       griddiv.style.height = '20px';
       griddiv.style.width = '20px';
       if (p1board[i][j] != null) {
@@ -40,13 +41,14 @@ export function renderGameBoards(game) {
   }
 
   const p2boardEl = document.querySelector('#gameboard-2');
-  p2boardEl.innerHTML = ''
+  p2boardEl.innerHTML = '';
   let p2board = game.player2.board.getBattleboard();
   for (let i = 0; i <= 9; i++) {
     for (let j = 0; j <= 9; j++) {
       const griddiv = document.createElement('div');
       griddiv.id = 'p2' + '-' + i + '-' + j;
-      griddiv.className = 'gameboard-cells outline outline-1 outline-slate-200 bg-slate-300 opacity-50 rounded-sm hover:opacity-100 hover:-translate-x-0.5 hover:-translate-y-0.5 transition';
+      griddiv.className =
+        'gameboard-cells outline outline-1 outline-slate-200 bg-slate-300 opacity-50 rounded-sm hover:opacity-100 hover:-translate-x-0.5 hover:-translate-y-0.5 transition';
       griddiv.style.height = '20px';
       griddiv.style.width = '20px';
       if (p2board[i][j] != null) {
